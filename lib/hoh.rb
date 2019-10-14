cddda3bf57f10fe07bf19a96b77e1bdd91e26df9
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 21:13:37 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,16 +54,7 @@ def naming_system
           label: "Family",
              sub_category: {
              label: "Genus",
-                 sub_category: nil
-                   label: "Species"
-                     
-            
-    }
-  }
- }
-     }
-   }
-  
- }
-  
+                 sub_category: {
+                   label: "Species",
+                   sub_category: nil } }}}}}}
 end
